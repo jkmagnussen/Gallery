4ffed2f60ae866e8700b633f8f9e5f1de5dfaefa
Joseph Karl Magnussen <jkmagnussen@outlook.com>
2021-09-29 01:25:56 +0100

    amended admin_content/ user class

---
+++ admin/includes/admin_content.php
@@ -0,0 +1,53 @@
+    <div class="container-fluid">
+
+        <!-- Page Heading -->
+        <div class="row">
+            <div class="col-lg-12">
+                <h1 class="page-header">
+                    Admin
+                    <small>Subheading</small>
+                </h1>
+
+                <?php   
+
+                // $result_set = User::find_all_users();
+                // while($row = mysqli_fetch_array($result_set)){
+                //     echo $row['username'] . "<br>";
+                // }
+
+                // $found_user = User::find_user_by_id(1);
+
+                // $user = User::instantiation($found_user );
+
+                // echo $user->username . "<br>";
+
+                // $users = User::find_all_users();
+
+                // foreach($users as $user){
+                //     echo $user->id . "<br>";
+                // }
+
+                $found_user = User::find_user_by_id(1);
+
+                echo $found_user->username;
+
+
+
+                
+                ?>
+
+
+                <ol class="breadcrumb">
+                    <li>
+                        <i class="fa fa-dashboard"></i> <a href="index.html">Dashboard</a>
+                    </li>
+                    <li class="active">
+                        <i class="fa fa-file"></i> Blank Page
+                    </li>
+                </ol>
+            </div>
+        </div>
+        <!-- /.row -->
+
+    </div>
+    <!-- /.container-fluid -->
