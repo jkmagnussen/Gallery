b25b8fc54f88c9df8a8afdd6f7f16412a6b63898
Joseph Karl Magnussen <jkmagnussen@outlook.com>
2021-11-16 17:21:54 +0000

    jjjjjj

--- admin/includes/admin_content.php
+++ admin/includes/admin_content.php
@@ -5,7 +5,7 @@
             <div class="col-lg-12">
                 <h1 class="page-header">
                     Admin
-                    <small>Dashboarrrrd</small>
+                    <small>Dashboarrd</small>
                 </h1>
 
                 <div class="row">
