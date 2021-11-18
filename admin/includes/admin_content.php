7259e1d3c9b5b8a1f007e7cc062a00b6b44b1cc9
Joseph Karl Magnussen <jkmagnussen@outlook.com>
2021-11-18 19:28:44 +0000

    severa;

--- admin/includes/admin_content.php
+++ admin/includes/admin_content.php
@@ -5,7 +5,7 @@
             <div class="col-lg-12">
                 <h1 class="page-header">
                     Admin
-                    <small>Dashboard</small>
+                    <small>Dashboooard</small>
                 </h1>
 
                 <div class="row">
